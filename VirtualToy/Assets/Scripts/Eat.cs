84dd9d6ef8bc8210bb3b59e18a869e7b56742ecd
Anthony Herrera <ankehecr100@gmail.com>
2019-06-05 21:34:28 -0500

    Juguete virtual

---
+++ VirtualToy/Assets/Scripts/Eat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Eat : MonoBehaviour
+{
+    private bool eaten = false;
+    public Text puntaje;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && !eaten)
+        {
+            eaten = !eaten;
+            this.gameObject.SetActive(false);
+            int x = 0;
+            int.TryParse(puntaje.text, out x);
+            x += 200;
+            puntaje.text = x.ToString();
+
+        }
+    }
+}
