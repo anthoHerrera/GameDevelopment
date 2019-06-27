02d02ad8988dacb37d8690679dfb90548301ef21
Anthony Herrera <ankehecr100@gmail.com>
2019-06-26 20:07:21 -0500

    Poniendo barrera de fin

---
+++ ExpVirtual/Assets/Scripts/Fin.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("fin");
+        this.GetComponent<BoxCollider2D>().isTrigger = false;
+    }
+}
